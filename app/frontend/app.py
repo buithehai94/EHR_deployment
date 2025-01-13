c1dc02dcfe67c6c6df7ad43ae20aeb6836c09c93
buithehai94 <buithehai329@gmail.com>
2025-01-13 16:58:06 +1100

    Update app.py

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -23,7 +23,7 @@ st.title('Patient Record Interaction App')
 @st.cache_data()
 def load_data():
     # Define the URL of the FastAPI endpoint
-    api_url = "https://fastapi-backend-ob2g.onrender.com/load_data/"
+    api_url = "https://ehr-api-8vsk.onrender.com/load_data/"
 
     # Make a GET request to the FastAPI endpoint
     response = requests.get(api_url)
